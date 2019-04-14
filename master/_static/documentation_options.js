70518e8bb38b3ba7b1da2636ffaf67ba2bb1b6cb
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-04-14 21:50:32 +0000

    Update docs after building Travis build 144 of areaDetector/areaDetector
    The docs were built from the branch 'master' against the commit
    4178ec919318678fd1ae017ae1aafd597cdb341d.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/areaDetector/areaDetector/jobs/192833383.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.3/bin/doctr deploy --key-path github_deploy_key_areadetector_areadetector_github_io.enc --build-tags --built-docs docs/_build/html --deploy-repo areaDetector/areaDetector.github.io master

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3-5-13-g5a5ee79',
+    VERSION: '3-5-14-g4178ec9',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
