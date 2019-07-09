45632404e352f17305b4269d52b0ee319aaf17fd
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-07-09 17:44:24 +0000

    Update docs after building Travis build 192 of areaDetector/areaDetector
    The docs were built from the branch 'master' against the commit
    223c1086b4e3622e8cfd1f70d345e8bef73118aa.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/areaDetector/areaDetector/jobs/214514515.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.3/bin/doctr deploy --key-path github_deploy_key_areadetector_areadetector_github_io.enc --build-tags --built-docs docs/_build/html --deploy-repo areaDetector/areaDetector.github.io master

---
+++ master/_sources/ADRIXSCam/ADRIXSCam.rst.txt
@@ -0,0 +1,7 @@
+areaDetector RIXSCAM driver
+===========================
+
+Overview
+--------
+
+
--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3-6-40-ga8b8cdc',
+    VERSION: '3-6-43-g223c108',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
